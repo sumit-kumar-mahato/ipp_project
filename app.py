277d1ba612f1e17e65f3eabfedf88f401a70ee5a
Sumit Kumar Mahato <sumitkrmht06@gmail.com>
2026-01-06 01:36:37 +0530

    Clean up imports in app.py
    Removed unused imports for cv2 and numpy.

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import numpy as np
-import cv2
-import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 from skimage.metrics import mean_squared_error, peak_signal_noise_ratio, structural_similarity
